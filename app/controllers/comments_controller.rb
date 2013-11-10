02ee07d0d2e2c4e9c8f8c37d8303b30c1804af2c
David Rabinowitz <daverabin@gmail.com>
2013-11-09 21:51:20 -0500

    added categories

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,17 @@
+class CommentsController < ApplicationController
+
+	def create
+		@post = Post.find(params[:post_id])
+		@comment = Comment.new(params.require(:comment).permit(:body))
+		@comment.post = @post
+		@comment.creator = User.first #TODO: fix after authentication
+
+		if @comment.save
+			flash[:notice] = "Your comment was added."
+			redirect_to post_path(@post)
+		else
+			render 'posts/show'
+		end
+
+	end
+end
